0633b64db0a30ae7d2c5c701fe38a0826d34b3d5
Benzazoua <billalben252@gmail.com>
2024-01-20 15:34:56 +0100

    Tag Creation Logic

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import NoteForm from "./NoteForm";
+import { NoteData, Tag } from "./App";
 
-function NewNote() {
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
   return (
     <>
       <h1>NewNote</h1>
-      <NoteForm />
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
     </>
   );
 }
